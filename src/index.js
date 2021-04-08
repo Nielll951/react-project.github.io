106c082d9581b1b80a81d4d8d8b2d44bdf1c6a06
Kostya <rodina9180gaocheng@ukr.net>
2021-04-08 21:29:16 +0300

    created react element through method createElement

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const el = React.createElement(
+  "h1",
+  null,
+  "Hello world"
+)
+
+ReactDOM.render(el,document.getElementById("root"))
