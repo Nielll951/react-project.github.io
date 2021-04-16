a109d76d634509ca977be6f73dd66b059e4e5d0c
Kostya <rodina9180gaocheng@ukr.net>
2021-04-16 17:51:22 +0300

    add products from products

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProductListItem from './ProductListItem'
+import products from './products'
 
 const ProductList = () => {
     return (
@@ -15,33 +16,25 @@ const ProductList = () => {
                         </div>
                     </div>
                     <div className="row post-row">
-                        <div className="col-xs-12 col-s-6 col-md-4">
-                            <ProductListItem
-                                name="ORGANIC CABBAGE/TIPS & GUIDES"
-                                title="Tips for Ripening your Fruit"
-                                description="Cabbage (comprising several cultivars of Brassica oleracea) is a leafy green, red (purple), or white (pale green) biennial plant"
-                                date="07"
-                                month="Nov"
-                            />
-                        </div>
-                        <div className="col-xs-12 col-s-6 col-md-4">
-                            <ProductListItem
-                                name="ORGANIC PUMPKIN/TIPS & GUIDES"
-                                title="Health Benefits of a Raw Food"
-                                description="A pumpkin is a cultivar of winter squash that is round with smooth, slightly ribbed skin, and is most often deep ye"
-                                date="11"
-                                month="Oct"
-                            />
-                        </div>
-                        <div className="col-xs-12 col-s-6 col-md-4">
-                            <ProductListItem
-                                name="ORGANIC WHEAT/TIPS & GUIDES"
-                                title="Superfoods you should eat"
-                                description="Wheat is a grass widely cultivated for its seed, a cereal grain which is a worldwide staple food. The many species "
-                                date="25"
-                                month="Sep"
-                            />
-                        </div>
+                        {
+                            products.map(({
+                                id,
+                                name,
+                                title,
+                                description,
+                                date,
+                                month
+                            }) => (
+                                <ProductListItem
+                                    key={id}
+                                    name={name}
+                                    title={title}
+                                    description={description}
+                                    date={date}
+                                    month={month}
+                                />
+                            ))
+                        }   
                     </div>
                 </div>
             </section> 
