318ab4d627132c83cb1e6b1530aed7a2f6b37bb8
Kostya <rodina9180gaocheng@ukr.net>
2021-04-24 14:46:12 +0300

    add Link to Logo.js

--- src/App/Header/Logo/Logo.js
+++ src/App/Header/Logo/Logo.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 import logo from "./logo.png"
 
 const Logo = () => {
     return (
         <div className="logo col-xs-6 col-md-2">
-            <a href="/" className="logo-link">
+            <Link to="/" className="logo-link">
                 <img src={logo} alt="logo" className="img-fluid"/>
-            </a>
+            </Link>
         </div> 
     )
 }
